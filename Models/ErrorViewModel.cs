eebb2c832bc0baf7ee5b99a910ed5215b85a1d56
Mazhar Saeed <mazhar@Mazhars-MBP.lan>
2023-10-23 14:13:41 +1100

    Adding some dot net code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace app_services_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
